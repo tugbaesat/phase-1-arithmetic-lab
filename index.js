92d7430ad26e0d7addaf38ca27668d2f714279b6
Tugba Esat <tugbaesat007@gmail.com>
2023-04-02 15:18:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1*num2;
+
+let random = Math.floor(Math.random()*10);
+let num3 = 24
+let num4 = 5
+const mod = (num3 % num4);
+const max = Math.max(4, 8, 7, 20, 5);
